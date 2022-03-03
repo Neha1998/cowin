48ebf6ac2d1b25ddaf7debd0a413a896418b4957
Neha1998 <singla.neha1998@gmail.com>
2022-03-03 21:51:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+- user registration(phone)
+- otpservice
+- slotbooking(location, age-group)
+	LIST (all centers)
+	GET (specific center details)
+	POST (customer_details= {} , ) Book the slot
+	DELETE
+	GET_BOOKED_SLOTS (list of all the slots booked) { secret code }
+- VACCINE_STATUS
+	GET - user's vaccine_ids (0, 1, 2)
+	POST - update user's status ("one dosed", "double dosed")
+
+- VACCINE (Covaxin, Covishield)
+- NOTIFICATION_SERVICE
+
+"""
